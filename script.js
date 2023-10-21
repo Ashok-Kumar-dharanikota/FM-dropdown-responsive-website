95d0b6699971b722e34bc706477788557bd18ae3
Ashok-Kumar-dharanikota <ashok81432@gmail.com>
2023-10-21 21:47:30 +0530

    aligned items correctly

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 const feature = document.getElementById('Features');
+const company = document.getElementById('Company');
+
 const ddone = document.getElementById("dd-one");
 const ddtwo = document.getElementById("dd-two");
 
-var count = 0;
+const ddArrowOne = document.getElementById('dd-one-arrow');
+const ddArrowTwo = document.getElementById('dd-two-arrow');
+
+var count1 = 0;
+var count2 = 0;
 
 // Add a click event listener
 feature.addEventListener('click', function () {
-    if (count == 0) {
+    if (count1 == 0) {
         ddone.style.display = "none";
-        count++;
+        ddArrowOne.src = "./images/icon-arrow-up.svg"
+        count1++;
     }
     else {
         ddone.style.display = "block";
-        count--;
+        ddArrowOne.src = "./images/icon-arrow-down.svg"
+        count1--;
     }
 });
 
+
+company.addEventListener('click', function () {
+    if (count2 == 0) {
+        ddtwo.style.display = "none";
+        ddArrowTwo.src = "./images/icon-arrow-up.svg"
+        count2++;
+    }
+    else {
+        ddtwo.style.display = "block";
+        ddArrowTwo.src = "./images/icon-arrow-down.svg"
+        count2--;
+    }
+});
